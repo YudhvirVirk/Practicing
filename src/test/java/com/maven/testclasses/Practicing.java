fb9481d690f6b3080cf96ea087f4ae4bafeff066
Yudhvir Virk <udvdjm@gmail.com>
2014-11-10 10:54:34 +0530

    mmm

---
+++ src/test/java/com/maven/testclasses/Practicing.java
@@ -0,0 +1,13 @@
+package com.maven.testclasses;
+
+import org.testng.annotations.Test;
+
+
+public class Practicing {
+
+	
+	@Test
+	public void test() throws Exception{
+		System.out.println("I m doin it finally");
+	}
+}
